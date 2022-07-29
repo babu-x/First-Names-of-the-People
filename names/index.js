400b9704f52d0bf5c140ed3551fe613a14e3c4f2
babu-x <bhaskar170863@gmail.com>
2022-07-29 10:23:22 +0000

    Test cases Faild

---
+++ names/index.js
@@ -0,0 +1,12 @@
+let names = require('../country/state/city/index.js');
+
+let firstNames = require('../utilities/utils/index.js');
+
+
+let getPeopleInCity = () => {
+console.log(firstNames(names))
+}
+module.exports = getPeopleInCity;
+
+
+
